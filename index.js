3ba1dc31c471685476eb55397b3279d86293138d
mouni-sri <mounikanagasri@gmail.com>
2024-06-25 15:46:55 +0530

    updated index.js file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain"});
-    res.end("Deploying NODEJS APP!");
+    res.end("Deploying NODEJS APP IN RENDER!");
 });
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, ()=>{
