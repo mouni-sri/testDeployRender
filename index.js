ff412f188344662f2486fefd09f6303ba0332d09
mouni-sri <mounikanagasri@gmail.com>
2024-06-25 15:18:25 +0530

    added index.js file

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require("http");
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain"});
+    res.end("Deploying NODEJS APP!");
+});
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, ()=>{
+    console.log("Server is running on the port 3000");
+});
